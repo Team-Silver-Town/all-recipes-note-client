97757b480960d78111159424872a1f5a8c0980a6
Wonil Nam <apotatos11@gmail.com>
2022-06-10 01:36:46 +0900

    fix: PageRecipes-Layout
    PageRecipe의 레이아웃을 개선하였습니다.

--- src/pages/PageRecipes.js
+++ src/pages/PageRecipes.js
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import styled from "styled-components";
+import { useQuery } from "react-query";
 
 import Header from "../components/Header";
-import Main from "../components/Main";
-import Footer from "../components/Footer";
-import { useQuery } from "react-query";
 import { getRecipes } from "../api/recipeApi";
 import { NavLink } from "react-router-dom";
 import RecipeCard from "../components/Card.Recipe";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function PageRecipes({ loginUserInfo, handleLogin }) {
   const { data: recipes } = useQuery("recipes", getRecipes);
@@ -21,13 +21,18 @@ function PageRecipes({ loginUserInfo, handleLogin }) {
     <Container>
       <Header loginUserInfo={loginUserInfo} handleLogin={handleLogin} />
       <Main>
-        <NavLink to="/recipes/new">영상추가하기</NavLink>
+        <StyledNewLink to="/recipes/new">
+          <div>
+            <FontAwesomeIcon icon={faPlus} size="4x" />
+          </div>
+          <div>새 영상 추가하기</div>
+        </StyledNewLink>
         {recipes &&
           recipes.map((recipe) => {
             return (
-              <NavLink id={recipe._id} to={`/recipes/${recipe._id}`}>
-                <RecipeCard recipeData={recipe} />;
-              </NavLink>
+              <StyledRecipeLink id={recipe._id} to={`/recipes/${recipe._id}`}>
+                <RecipeCard recipeData={recipe} />
+              </StyledRecipeLink>
             );
           })}
       </Main>
@@ -41,4 +46,49 @@ const Container = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
+`;
+
+const Main = styled.main`
+  height: 100%;
+  overflow: auto;
+  padding-top: 80px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-auto-rows: 300px;
+  gap: 10px;
+  margin: 10px;
+`;
+
+const StyledNewLink = styled(NavLink)`
+  display: block;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  transition: all ease 1s;
+
+  div:nth-child(1) {
+    transition: all ease-in-out 0.5s;
+  }
+
+  div:nth-child(1):hover {
+    transform: rotate(90deg);
+  }
+
+  div:nth-child(2) {
+    position: absolute;
+    bottom: 10px;
+    font-size: medium;
+  }
+`;
+
+const StyledRecipeLink = styled(NavLink)`
+  display: block;
+  width: 100%;
+  height: 100%;
+  background-color: white;
 `;
